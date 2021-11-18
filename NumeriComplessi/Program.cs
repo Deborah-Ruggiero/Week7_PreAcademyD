4d7f1fa4b5c8268469687f9dc70bae220e9b2f15
Renata Carriero <Renata.Carriero@icubed.it>
2021-11-18 17:31:05 +0100

    Aggiornato progetto NumeriComplessi

--- NumeriComplessi/Program.cs
+++ NumeriComplessi/Program.cs
@@ -6,40 +6,32 @@ namespace NumeriComplessi
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Divisione tra numeri complessi!");
-            //Console.WriteLine("Inserisci parte reale del primo numero complesso");
-            //double.TryParse(Console.ReadLine(), out double a);
-            //Console.WriteLine("Inserisci parte immaginaria del primo numero complesso");
-            //double.TryParse(Console.ReadLine(), out double b);
-            //Console.WriteLine("Inserisci parte reale del secondo numero complesso");
-            //double.TryParse(Console.ReadLine(), out double c);
-            //Console.WriteLine("Inserisci parte immaginaria del secondo numero complesso");
-            //double.TryParse(Console.ReadLine(), out double d);
+            Console.WriteLine("Divisione tra numeri complessi!");
+            Console.WriteLine("Inserisci parte reale del primo numero complesso");
+            double.TryParse(Console.ReadLine(), out double a);
+            Console.WriteLine("Inserisci parte immaginaria del primo numero complesso");
+            double.TryParse(Console.ReadLine(), out double b);
+            Console.WriteLine("Inserisci parte reale del secondo numero complesso");
+            double.TryParse(Console.ReadLine(), out double c);
+            Console.WriteLine("Inserisci parte immaginaria del secondo numero complesso");
+            double.TryParse(Console.ReadLine(), out double d);
 
-            //NumeroComplesso primo = new NumeroComplesso { ParteReale = a , ParteImmaginaria=b};
-            //NumeroComplesso secondo = new NumeroComplesso { ParteReale = c , ParteImmaginaria=d};
-            ////try
-            ////{
-            //    NumeroComplesso risultato = primo.Divisione(secondo);
-            //    Console.WriteLine(risultato);
-            ////}
-            ////catch(NumeroComplessoException ncex)
-            ////{
-            ////    Console.WriteLine(ncex.Message);
-            ////    Console.WriteLine($"Dividendo: {ncex.PrimoOperatore} - Divisore: {ncex.SecondoOperatore}");
-
+            NumeroComplesso primo = new NumeroComplesso { ParteReale = a, ParteImmaginaria = b };
+            NumeroComplesso secondo = new NumeroComplesso { ParteReale = c, ParteImmaginaria = d };
+            //try
+            //{
+            NumeroComplesso risultato = primo.Divisione(secondo);
+            Console.WriteLine(risultato);
             //}
-            try
-            {
-                int a = 0;
-                Console.WriteLine(6 / a);
-            }
-            finally
-            {
-                Console.WriteLine("Finally");
-            }
-            
+            //catch(NumeroComplessoException ncex)
+            //{
+            //    Console.WriteLine(ncex.Message);
+            //    Console.WriteLine($"Dividendo: {ncex.PrimoOperatore} - Divisore: {ncex.SecondoOperatore}");
 
         }
+
+
+
+    }
     }
 }
